fa6ffe2e19dbbc4c8e62436098e56235b8f1e809
Georgi Tapalilov <tapalilov@gmail.com>
2013-12-20 14:34:24 +0200

    [grading_methods/lib]

---
+++ core/lib/gaku/grading_methods/base_method.rb
@@ -0,0 +1,15 @@
+module Gaku
+  module GradingMethods
+    class  BaseMethod
+
+      def initialize(exam, students)
+        @exam = exam
+        @students = students
+      end
+
+      def grade
+      end
+
+    end
+  end
+end
--- core/lib/gaku/grading_methods/score.rb
+++ core/lib/gaku/grading_methods/score.rb
@@ -1,20 +1,20 @@
-module Gaku
-  module GradingMethods
-    class  Score
+class  Gaku::GradingMethods::Score < Gaku::GradingMethods::BaseMethod
 
-      def initialize(exam, students)
-        @exam = exam
-        @students = students
-      end
-
-      def grade
-        [@exam, @students]
-      end
-
-      def get_grade(key)
-        @students[key]
-      end
-
-    end
+  def initialize(exam, students)
+    super
   end
+
+  def grade
+   #EXAMPLE results: exam_id: { student_id: 5 }
+    results = Hash.new { |hash,key| hash[key] = {} }
+    @students.each do |student|
+      results[@exam.id][student.id] = nil
+      @exam.exam_portions.each do |exam_portion|
+        exam_portion_score = student.exam_portion_scores.where(exam_portion_id: exam_portion.id).first.try(:score)
+        results[@exam.id][student.id] = results[@exam.id][student.id].to_f +  exam_portion_score.to_f if exam_portion_score.present?
+      end
+    end
+    return results
+  end
+
 end
